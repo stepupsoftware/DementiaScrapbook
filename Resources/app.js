71e937d9571d14ecbf9704561162b6b8787803b2
Paul Ryan <paul@sharpred.com>
2013-01-28 18:44:30 +0000

    added testflight

--- Resources/app.js
+++ Resources/app.js
@@ -1,14 +1,14 @@
 var joli = require('libs/joli').connect('dementia001');
 var Flurry = require('ti.flurry');
-
 Flurry.debugLogEnabled = true;
 Flurry.eventLoggingEnabled = true;
-
 Flurry.initialize('3CG3RFQT8ZQQY2NP3V5S');
-
 Flurry.reportOnClose = true;
 Flurry.sessionReportsOnPauseEnabled = true;
 Flurry.secureTransportEnabled = false;
+
+var testflight = require('com.0x82.testflight');
+testflight.takeOff('3bd615ceb2e565ddd53e8035e1c95e9c_MTc5NDM0MjAxMy0wMS0yOCAwNjo0NTo0NC43MjAxNTk');
 
 osname = Titanium.Platform.osname;
 if (osname === 'ipad') {
