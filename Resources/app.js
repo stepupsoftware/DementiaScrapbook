eeadaf4c41b45e24e675024583e476daa4553644
Paul Ryan <paul@sharpred.com>
2013-01-28 11:54:28 +0000

    added flurry support

--- Resources/app.js
+++ Resources/app.js
@@ -1,8 +1,19 @@
 var joli = require('libs/joli').connect('dementia001');
+var Flurry = require('ti.flurry');
+
+Flurry.debugLogEnabled = true;
+Flurry.eventLoggingEnabled = true;
+
+Flurry.initialize('3CG3RFQT8ZQQY2NP3V5S');
+
+Flurry.reportOnClose = true;
+Flurry.sessionReportsOnPauseEnabled = true;
+Flurry.secureTransportEnabled = false;
+
 osname = Titanium.Platform.osname;
 if (osname === 'ipad') {
-	var splitView = new ( require('SplitView').SplitView)(this);
-	splitView.open();
+    var splitView = new ( require('SplitView').SplitView)(this);
+    splitView.open();
 } else if (osname === 'android' || osname === 'iphone') {
-	var splitView = new ( require('SingleView').SingleWindow)(this);
+    var splitView = new ( require('SingleView').SingleWindow)(this);
 }
