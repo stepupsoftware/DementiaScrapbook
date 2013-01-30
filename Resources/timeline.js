6dfbf539404275c21aec62e30f18fa49d9bea39e
Paul Ryan <paul@sharpred.com>
2013-01-30 15:22:15 +0000

    added canned events json file

--- Resources/timeline.js
+++ Resources/timeline.js
@@ -1,38 +1,33 @@
 /*global L, Ti, Titanium, joli, uploader, logger, models, sus, cust*/
 /*jslint nomen: true, sloppy : true, plusplus: true, vars: true, newcap: true*/
 function timeline() {
-    win = Ti.UI.createWindow({
-        title : L('timeline'),
-        backgroundColor : '#696969',
-        id : "DETAIL_WINDOW",
-        tabBarHidden : true,
-        layout : 'composite'
+    win = Ti.UI.createWindow(_.extend({
+        title : L('timeline')
+    }, theme.window));
 
-    });
-    
     tv = Titanium.UI.createTableView();
-    
+
     var addRows = function() {
         var data = [];
         var today = Ti.UI.createTableViewSection({
-            headerTitle: L('today')
+            headerTitle : L('today')
         });
         var visit = Ti.UI.createTableViewRow({
-            title: 'Visit from Lisa'
+            title : 'Visit from Lisa'
         });
         today.add(visit);
         var tomorrow = Ti.UI.createTableViewSection({
-            headerTitle: L('tomorrow')
+            headerTitle : L('tomorrow')
         });
         var lunch = Ti.UI.createTableViewRow({
-            title: 'You have lunch today'
+            title : 'You have lunch today'
         });
         tomorrow.add(lunch);
         var yesterday = Ti.UI.createTableViewSection({
-            headerTitle: L('yesterday')
+            headerTitle : L('yesterday')
         });
         var bart = Ti.UI.createTableViewRow({
-            title: 'Bart visited yesterday'
+            title : 'Bart visited yesterday'
         });
         yesterday.add(bart);
         data.push(yesterday);
@@ -42,7 +37,7 @@ function timeline() {
     };
     win.add(tv);
     tv.setData(addRows());
-        tv.addEventListener('click', function() {
+    tv.addEventListener('click', function() {
         alert('detail on event will appear here');
     });
     win.addEventListener('focus', function() {
