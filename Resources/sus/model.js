eb4a7deeb4e9e949b7a5bb434108030ea5397c9b
Paul Ryan <paul@sharpred.com>
2013-03-24 21:25:19 +0000

    up to date model file

--- Resources/sus/model.js
+++ Resources/sus/model.js
@@ -5,26 +5,53 @@ if (!_) {
     var _ = require('lib/underscore');
 }
 
+//this constructor is a facade to the titaffydb library.
 var Model = function(args) {
     //private objects
-    var taffy, db, msg, merge, get, first, extract, dbname;
+    var taffy, db, msg, get, getField, first, insert, merge, stringify, sync, dbname, onUpdate, onRemove;
 
     try {
         taffy = require('lib/taffydb4ti').taffy;
-        dbname = args;
+        dbname = args.db;
         db = new taffy(dbname);
-        db.settings = {
-            onUpdate : function(e) {
-                var msg = JSON.stringify(e);
+        //onUpdate, onInsert and onRemove are callbacks that can be specified when updating taffyDB records
+        if (!args.onUpdate || (typeof args.onUpdate !== 'function')) {
+            onUpdate = function() {
+                var msg = 'record ' +this.___id +' updated in ' +dbname + JSON.stringify(this);
                 Ti.API.debug(msg);
-            },
-            onRemove : function(e) {
-                var msg = JSON.stringify(e);
+            };
+        } else {
+            onUpdate = args.onUpdate;
+        }
+
+        if (!args.onInsert || (typeof args.onInsert !== 'function')) {
+            onInsert = function() {
+                var msg = 'new record inserted in ' +dbname + JSON.stringify(this);
                 Ti.API.debug(msg);
-            }
-        };
-        first = function() {
+            };
+        } else {
+            onInsert = args.onInsert;
+        }
+        if (!args.onRemove || (typeof args.onRemove !== 'function')) {
+            onRemove = function() {
+                var msg = 'record removed from ' +dbname + JSON.stringify(this);
+                Ti.API.debug(msg);
+            };
+        } else {
+            onRemove = args.onRemove;
+        }
+
+        db.settings({
+            onUpdate : onUpdate,
+            onInsert : onInsert,
+            onRemove : onRemove
+        });
+        //private functions and objects
+        first = function(args) {
             try {
+                if (args) {
+                    return db(args).first();
+                }
                 return db().first();
 
             } catch (ex) {
@@ -45,11 +72,15 @@ var Model = function(args) {
             }
         };
 
-        get = function() {
+        get = function(args) {
+            if (args) {
+                return db(args).get();
+            }
             return db().get();
+
         };
 
-        insert = function(args) {
+        insert = function(args, runEvent) {
             try {
                 db.insert(args);
                 db.save();
@@ -64,43 +95,51 @@ var Model = function(args) {
             var count;
             db.merge(data, key, runEvent);
             db.save();
-            count = db().filter().count();
-            Ti.API.info('there are ' + count + ' ' + dbname + ' records');
-            //Ti.API.info('data' + db().stringify());
+            Ti.API.debug('model ' +dbname +' has ' +db().count() +' records');
         };
-
-        extract = function() {
-            db().stringify();
-        };
-        //public objects (defined with this)
-
-        //returns a string of JSON text
-        this.dbname = dbname;
-        this.extract = extract;
-
-        this.find = function(args) {
+        
+        remove = function() {
             try {
-
-                return db().get(args);
-
+                if (args) {
+                    db(args).remove(true);
+                }
+                db().remove(true);
+                db.save();
             } catch (ex) {
                 var msg = ex.message || ex;
                 Ti.API.error(msg);
             }
         };
+
+        stringify = function(args) {
+            try {
+                if (args) {
+                    return db(args).stringify();
+                }
+                return db().stringify();
+
+            } catch (ex) {
+                var msg = ex.message || ex;
+                Ti.API.error(msg);
+                return undefined;
+            }
+
+        };
+        
+        
+
+        //public functions objects
+        this.dbname = dbname;
+        this.find = get;
         this.first = first;
         this.get = get;
         this.getField = getField;
         this.insert = insert;
-
+        this.stringify = stringify;
         this.merge = merge;
-        this.remove = function(args) {
-            
-            //TODO remove needs to search for an item, return its ID and then remove it
-            db().remove(args, true);
-            db.save();
-
-        };
+        this.remove = remove;
+        this.sync = sync;
+        
     } catch (ex) {
 
         msg = ex.message || ex || 'unknown';
@@ -120,12 +159,26 @@ var Model = function(args) {
 };
 
 var create = function(args) {
-    var msg, model;
+    var msg, model, settings = {};
     try {
         if (!args.db) {
             throw new error('db not defined in: ' + JSON.stringify(args));
+        } else {
+            settings.db = args.db;
         }
-        model = new Model(args.db);
+        if (args.callbacks) {
+            //add callbacks
+                _.each(args.callbacks, function(method, key) {
+                    //callback must be a function called onUpdate, onInsert or onRemove
+                    if ( typeof method === 'function' && typeof key === 'string' && (key === 'onInsert' || key === 'onUpdate' || key === 'onRemove')) {
+                        settings[key] = method;
+                    } else {
+                        Ti.API.error('invalid callback ' +key +'specified');
+                    }
+                });
+        }
+        //TODO add insert, update and remove callbacks
+        model = new Model(settings);
         //object decoration here
         if (model && ( model instanceof Model)) {
             //add supplied default values
@@ -141,6 +194,8 @@ var create = function(args) {
                             Ti.API.info('function ' + key + ' has been overloaded for model ' + args.db);
                         }
                         model[key] = method;
+                    } else {
+                        Ti.API.error('invalid method ' +key +'specified');
                     }
                 });
             }
@@ -149,6 +204,8 @@ var create = function(args) {
                 _.each(args.properties, function(value, property) {
                     if ( typeof property === 'string') {
                         model[property] = value;
+                    } else {
+                        Ti.API.error('invalid property ' +property +'specified');
                     }
                 });
             }
